5a80462d080acc017ceca951b784e55e9bc76e72
kaiTran56 <quyettran057@gmail.com>
2021-03-03 14:04:18 +0700

    new updated code!

--- src/a1_1801040175/Math.java
+++ src/a1_1801040175/Math.java
@@ -31,6 +31,24 @@ public class Math {
 		}
 	}
 
+	/**
+	 * determine the midle one of three numbers
+	 * 
+	 * @requires: a != null /\ b != null /\ c != null
+	 * 
+	 * 
+	 * @effects: return the number which is in the midle of three number
+	 * 
+	 *           <pre>
+	 *           i.e: if ((a < b && b < c) || (c < b && b < a)) return b;
+	 * 
+	 *           else if ((b < a && a < c) || (c < a && a < b)) return a;
+	 * 
+	 *           else return c;
+	 * 
+	 *           <pre>
+	 *           e.g: middle(8,9,10) = 9
+	 */
 	public static int middle(int a, int b, int c) {
 
 		if ((a < b && b < c) || (c < b && b < a))
@@ -43,9 +61,23 @@ public class Math {
 			return c;
 	}
 
+	/**
+	 * determine the string is a palindrom or not
+	 * 
+	 * @param str !=null
+	 * @effects: return the determine of string is palindrom or not
+	 * 
+	 *           <pre>
+	 *           i.e:if(str.length()==1) return true; <br>
+	 *           \/ check the symbol per position of str which have the same
+	 *           character with the opersite characters
+	 */
 	public static boolean isPalindrome(String str) {
 		int i = 0, j = str.length() - 1;
 
+		if (str.length() == 1) {
+			return true;
+		}
 		while (i < j) {
 
 			if (str.charAt(i) != str.charAt(j))
@@ -56,6 +88,17 @@ public class Math {
 
 		return true;
 	}
+
+	/**
+	 * determine the integer number is prime or not
+	 * 
+	 * @requires: number >0
+	 * @effects:
+	 * 
+	 *           <pre>
+	 *           if(1<=number<=2) return true <br>
+	 *           forall i in the range[2, number)/\ if(number % i ==0) return false
+	 */
 
 	public static boolean isPrime(int number) {
 		if (number <= 2 && number >= 1) {
