76e1d62289de70db8fbc151e0cafb79401dd97d1
kaiTran56 <quyettran057@gmail.com>
2021-03-17 17:10:42 +0700

    new updated code!

--- src/tut06/MobilePhone.java
+++ src/tut06/MobilePhone.java
@@ -1,5 +1,55 @@
 package tut06;
 
+import utils.DomainConstraint;
+
 public class MobilePhone {
+
+	@DomainConstraint(type = "Integer", mutable = false, optional = false, length = 30)
+	private String manName;
+	@DomainConstraint(type = "String", mutable = false, optional = false, length = 15)
+	private String model;
+	private char color;
+	private int year;
+	private boolean guaranteed;
+
+	public String getManName() {
+		return manName;
+	}
+
+	public void setManName(String manName) {
+		this.manName = manName;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public char getColor() {
+		return color;
+	}
+
+	public void setColor(char color) {
+		this.color = color;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public boolean isGuaranteed() {
+		return guaranteed;
+	}
+
+	public void setGuaranteed(boolean guaranteed) {
+		this.guaranteed = guaranteed;
+	}
 
 }
--- src/tut06/Person.java
+++ src/tut06/Person.java
@@ -1,7 +1,11 @@
 package tut06;
 
 import tut05.MobilePhone;
+import utils.AttrRef;
+import utils.DOpt;
 import utils.DomainConstraint;
+import utils.NotPossibleException;
+import utils.OptType;
 
 public class Person {
 
@@ -12,35 +16,77 @@ public class Person {
 	@DomainConstraint(type = "MobilePhone", mutable = true, optional = true)
 	private MobilePhone phone;
 
-	public Person(int id, String name, MobilePhone phone) {
+	public Person(@AttrRef("id") int id, @AttrRef("name") String name) throws NotPossibleException {
 		this.id = id;
 		this.name = name;
-		this.phone = phone;
 
 	}
 
+	@DOpt(type = OptType.Observer)
+	@AttrRef("id")
 	public int getId() {
 		return id;
 	}
 
+	@DOpt(type = OptType.Mutator)
+	@AttrRef("id")
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	@DOpt(type = OptType.Observer)
+	@AttrRef("name")
 	public String getName() {
 		return name;
 	}
 
+	@DOpt(type = OptType.Mutator)
+	@AttrRef("name")
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	@DOpt(type = OptType.Observer)
+	@AttrRef("phone")
 	public MobilePhone getPhone() {
 		return phone;
 	}
 
+	@DOpt(type = OptType.Mutator)
+	@AttrRef("phone")
 	public void setPhone(MobilePhone phone) {
 		this.phone = phone;
+	}
+
+	@DOpt(type = OptType.Helper)
+	@AttrRef("id")
+	private boolean validateId(int id) {
+		return false;
+	}
+
+	@DOpt(type = OptType.Helper)
+	@AttrRef("name")
+	private boolean validateName(String name) {
+		return false;
+	}
+
+	@DOpt(type = OptType.Helper)
+	public boolean repOk() {
+		return false;
+	}
+
+	@Override
+	@DOpt(type = OptType.Default)
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+	@Override
+	@DOpt(type = OptType.Default)
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
 	}
 
 }
