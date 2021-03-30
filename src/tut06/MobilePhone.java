69ad651553bda1c3ecfc17f8a6e6ef7b271d79aa
kaiTran56 <quyettran057@gmail.com>
2021-03-30 10:43:16 +0700

    new update code !

--- src/tut06/MobilePhone.java
+++ src/tut06/MobilePhone.java
@@ -57,8 +57,25 @@ public class MobilePhone {
 
 	@DOpt(type = OptType.Constructor)
 	public MobilePhone(@AttrRef("manName") String manName, @AttrRef("model") String model, @AttrRef("color") char color,
-			@AttrRef("year") int year, @AttrRef("guaranteed") boolean guarenteed) throws NotPossibleException {
+			@AttrRef("year") int year, @AttrRef("guaranteed") boolean guaranteed) throws NotPossibleException {
+		if (!validateManName(manName)) {
+			throw new NotPossibleException("MobilePhone.init: invalid mobile phone manName: " + manName);
+		}
+		if (!validateModel(model)) {
+			throw new NotPossibleException("MobilePhone.init: invalid mobile phone model: " + model);
+		}
+		if (!validateColor(color)) {
+			throw new NotPossibleException("MobilePhone.init: invalid mobile phone color: " + color);
+		}
+		if (!validateYear(year)) {
+			throw new NotPossibleException("MobilePhone.init: invalid mobile phone year: " + year);
+		}
 
+		this.manName = manName;
+		this.model = model;
+		this.color = color;
+		this.year = year;
+		this.guaranteed = guaranteed;
 	}
 
 	/**
@@ -110,6 +127,105 @@ public class MobilePhone {
 		} else {
 			return false;
 		}
+	}
+
+	@DOpt(type = OptType.Observer)
+	@AttrRef("color")
+	public char getColor() {
+		return color;
+	}
+
+	@DOpt(type = OptType.Mutator)
+	@AttrRef("color")
+	public boolean setColor(char color) {
+		if (validateColor(color)) {
+			this.color = color;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@DOpt(type = OptType.Observer)
+	@AttrRef("year")
+	public int getYear() {
+		return year;
+	}
+
+	@DOpt(type = OptType.Mutator)
+	@AttrRef("year")
+	public boolean setYear(int year) {
+		if (validateYear(year)) {
+			this.year = year;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@DOpt(type = OptType.Observer)
+	@AttrRef("guaranteed")
+	public boolean getGuaranteed() {
+		return guaranteed;
+	}
+
+	@DOpt(type = OptType.Mutator)
+	@AttrRef("guaranteed")
+	public boolean setGuaranteed(boolean guaranteed) {
+		this.guaranteed = guaranteed;
+		return true;
+	}
+
+	@DOpt(type = OptType.Helper)
+	@AttrRef("manName")
+	private boolean validateManName(String manName) {
+		return manName != null && manName.length() <= 30;
+	}
+
+	@DOpt(type = OptType.Helper)
+	@AttrRef("model")
+	private boolean validateModel(String model) {
+		return model != null && model.length() <= 15;
+	}
+
+	@DOpt(type = OptType.Helper)
+	@AttrRef("color")
+	private boolean validateColor(char color) {
+		char[] validColor = { 'R', 'O', 'Y', 'B', 'P' };
+		for (char temp : validColor) {
+			if (color == temp) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@DOpt(type = OptType.Helper)
+	@AttrRef("year")
+	private boolean validateYear(int year) {
+		return year >= 1970;
+	}
+
+	@DOpt(type = OptType.Helper)
+	public boolean repOk() {
+		return validateColor(color) && validateManName(manName) && validateModel(model) && validateYear(year);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null || !(obj instanceof MobilePhone)) {
+			return false;
+		} else {
+			MobilePhone other = (MobilePhone) obj;
+			return manName.equals(other.getManName()) && model.equals(other.getModel()) && year == other.getYear()
+					&& color == other.getColor();
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "MobilePhone:<" + manName + ", " + model + ", " + color + ", " + year + ", guarannteed: " + guaranteed
+				+ ">";
 	}
 
 }
